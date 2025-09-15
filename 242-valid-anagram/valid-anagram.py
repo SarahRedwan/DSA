901528bfb51ceb88a5bde19677fe934b3ec3caa5
Sara Redwan Shure <sara.redwan@aastustudent.edu.et>
2025-09-15 20:48:22 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (34.20%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+    
+        unique_chars = set(s)
+        
+        for i in unique_chars:
+            if s.count(i) != t.count(i):
+                return False
+                
+        return True
