1a378131f8901be3918aaf0790339ca4f6d67d94
Sara Redwan Shure <susured22@gmail.com>
2026-02-26 17:01:53 +0300

    Time: 197 ms (20.22%) | Memory: 20.1 MB (15.56%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        if len(s) < len(p):   # guard clause
+        if len(s) < len(p):
             return []
 
         p_counter = Counter(p)
@@ -11,18 +11,17 @@ class Solution:
         result = []
         k = len(p)
 
-        # Step 1: initialize the first window
+        
         for i in range(k):
             window_counter[s[i]] += 1
         if window_counter == p_counter:
             result.append(0)
 
         left = 0
-        # Step 2: slide the window
         for right in range(k, len(s)):
             window_counter[s[right]] += 1
             window_counter[s[left]] -= 1
-            if window_counter[s[left]] == 0:
+            if window_counter[s[left]] == 0: 
                 del window_counter[s[left]]
             left += 1
 
