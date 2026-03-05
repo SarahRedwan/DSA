e412ee100fd7df49ce568ba94c2d1d2e8360f99d
Sara Redwan Shure <susured22@gmail.com>
2026-03-06 00:04:47 +0300

    Time: 154 ms (17.85%) | Memory: 22.2 MB (28.04%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.py
@@ -1,34 +1,27 @@
 class Solution:
-    def subarraysWithKDistinct(self, nums: list[int], k: int) -> int:
-        
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        left1=0
+        left2=0
+        dict1=defaultdict(int)
+        dict2=defaultdict(int)
 
-        left1 = 0
-        count1 = 0
-        dict1 = defaultdict(int)
-        
-        left2 = 0
-        count2 = 0
-        dict2 = defaultdict(int)
+        count1=0
+        count2=0
 
-        for right in range(len(nums)):
+        for r in range(len(nums)):
+            dict1[nums[r]]+=1
+            dict2[nums[r]]+=1
 
-            dict1[nums[right]] += 1
-            dict2[nums[right]] += 1
-
-            while len(dict1) > k:
-                dict1[nums[left1]] -= 1
-                if dict1[nums[left1]] == 0:
+            while len(dict1)>k:
+                dict1[nums[left1]]-=1
+                if dict1[nums[left1]]==0:
                     del dict1[nums[left1]]
-                left1 += 1
-
-
-            while len(dict2) > k - 1:
-                dict2[nums[left2]] -= 1
-                if dict2[nums[left2]] == 0:
+                left1+=1
+            while len(dict2)>k-1:
+                dict2[nums[left2]]-=1
+                if dict2[nums[left2]]==0:
                     del dict2[nums[left2]]
-                left2 += 1
-
-            count1 += right - left1 + 1
-            count2 += right - left2 + 1
-
-        return count1 - count2
+                left2+=1
+            count1+=r-left1+1
+            count2+=r-left2+1
+        return count1-count2
