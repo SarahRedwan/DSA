4babffb812ab78b2cc07a2a630b0fe97aff12d37
Sara Redwan Shure <susured22@gmail.com>
2026-03-11 17:57:06 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (82.75%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def scoreOfParentheses(self, s: str) -> int:
+        stack=[0]
+        for c in s:
+            if c=="(":
+                stack.append(0)
+            else:
+                V=stack.pop()
+
+                if V==0:
+                    score=1
+                else:
+                    score=V*2
+                stack[-1]+=score
+
+        return stack[0]
+            
+            
