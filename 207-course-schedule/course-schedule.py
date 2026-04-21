d898dd1499a04728274a733f9be4c662de6c594b
Sara Redwan Shure <susured22@gmail.com>
2026-04-21 19:30:10 +0300

    Time: 3 ms (87.53%) | Memory: 21.7 MB (9.16%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph=defaultdict(list)
+        
+
+        for a,b in prerequisites:
+            graph[a].append(b)
+        visiting=set()
+        visited=set()
+        def dfs(course):
+            if course in visiting:
+                return False
+            if course in visited:
+                return True
+            
+            visiting.add(course)
+
+            for pre in graph[course]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(course)
+            visited.add(course)
+            return True
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        
+        return True
+
