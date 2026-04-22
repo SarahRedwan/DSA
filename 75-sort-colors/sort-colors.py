90caaeeb7609460b1b1aadac1a2e12a0930a671f
Sara Redwan Shure <susured22@gmail.com>
2026-04-22 17:28:59 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.74%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,8 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-    
-        low,mid,high=0,0,len(nums)-1
+        low=0
+        mid=0
+        high=len(nums)-1
+
         while mid<=high:
             if nums[mid]==0:
                 nums[low],nums[mid]=nums[mid],nums[low]
@@ -16,5 +18,3 @@ class Solution:
                 nums[mid],nums[high]=nums[high],nums[mid]
                 high-=1
 
-
-        
